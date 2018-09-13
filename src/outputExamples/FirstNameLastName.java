fd859e1957b7a7f0b6b7fecf5846a2ab9926f708
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-09-13 13:10:32 -0500

    FirstNameLastName

---
+++ src/outputExamples/FirstNameLastName.java
@@ -0,0 +1,11 @@
+package outputExamples;
+
+public class FirstNameLastName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Carli");
+System.out.println("Ramon");
+	}
+
+}
