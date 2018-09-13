95ea9766220fb213c37ee16814798c44912924fc
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-09-13 13:16:52 -0500

    FirstNameLastNameAge

---
+++ src/outputExamples/FirstNameLastNameAge.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class FirstNameLastNameAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Carli");
+	System.out.println("Ramon");
+	System.out.println("17");
+	}
+
+}
