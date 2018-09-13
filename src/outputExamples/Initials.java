f963794934c58aadd7ca61ffb61b41c563426c64
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-09-13 14:09:07 -0500

    Initials

---
+++ src/outputExamples/Initials.java
@@ -0,0 +1,22 @@
+package outputExamples;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("CCCCCC  CCCCCC  RRRRRR");
+
+System.out.println("C       C       R    R");
+
+System.out.println("C       C       R   R");
+
+System.out.println("C       C       RRRR");
+
+System.out.println("C       C       R    R");
+
+System.out.println("C       C       R     R");
+
+System.out.println("CCCCCC  CCCCCC  R      R");
+	}
+
+}
