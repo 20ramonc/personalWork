389e4b14e7453f9a741b3c6a447c82195244173c
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-09-13 13:57:12 -0500

    Likes and Dislilkes

---
+++ src/outputExamples/LikesAndDislikes.java
@@ -0,0 +1,14 @@
+package outputExamples;
+
+public class LikesAndDislikes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Likes \t \t Dislikes");
+System.out.println("Math \t \t English");
+System.out.println("Music \t \t Reading");
+System.out.println("\t \t Spiders");
+
+	}
+
+}
