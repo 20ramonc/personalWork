c3ab4ba375f110fb3f2e3b799412228192f17011
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-09-20 13:07:37 -0500

    FinalMonkey

--- src/inputExamples/Monkey.java
+++ src/inputExamples/Monkey.java
@@ -7,7 +7,7 @@ public class Monkey {
 		Scanner input = new Scanner(System.in);
 System.out.println("What is height of an adult Monkey?");
 monkey = input.nextDouble();
-System.out.println("The height of an adult Monkey is" + monkey+ "meters tall.");
+System.out.println("The height of an adult Monkey is " + monkey+ " meters tall.");
 
 	}
 
