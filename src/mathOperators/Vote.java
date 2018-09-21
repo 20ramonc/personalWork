707a115680cbb1e41ead6455200c0ec2f0300c31
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-09-21 14:35:21 -0500

    YeartoVote

---
+++ src/mathOperators/Vote.java
@@ -0,0 +1,15 @@
+package mathOperators;
+import java.util.Scanner;
+public class Vote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int vote = -999;
+Scanner input = new Scanner(System.in);
+System.out.println("How many years till you are able to vote?");
+vote = input.nextInt() + 1;
+System.out.println("In 1 year I can vote");
+
+	}
+
+}
