6769c86bc76d36f07ab3f6f069cbaf44ffac9c3c
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-10-01 14:45:54 -0500

    Friend

--- src/oOP/Friend.java
+++ src/oOP/Friend.java
@@ -1,5 +1,16 @@
 package oOP;
 
-public class Friend {
+public class Friend 
+{
 
+//instance fields
+//default constructor
+public Friend() 
+{
 }
+
+//methods
+public void sayMessage()
+{
+	System.out.println("You are a great friend");
+}}
