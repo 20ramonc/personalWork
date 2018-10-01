9fe5cea941b5650a86e62b8eb70c6241bbffcf4f
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-10-01 14:47:04 -0500

    GreeterTestClass

--- src/oOP/GreaterTestClass.java
+++ src/oOP/GreaterTestClass.java
@@ -4,7 +4,16 @@ public class GreaterTestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Greater bob = new Greater();
+bob.sayHello();
+Friend john = new Friend ();
+john.sayMessage();
+Friend jenny = new Friend ();
+Friend billy = new Friend ();
+Friend sammy = new Friend ();
+Friend kyle = new Friend ();
+billy.sayMessage();
+kyle.sayMessage();
 	}
 
 }
