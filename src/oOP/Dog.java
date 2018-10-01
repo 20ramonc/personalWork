442f8ab15d0565e1ea2c1da7df2dc676e8e7055f
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-10-01 15:01:16 -0500

    Dog

---
+++ src/oOP/Dog.java
@@ -0,0 +1,12 @@
+package oOP;
+
+public class Dog {
+public Dog ()
+{
+}
+public void woof () {
+	System.out.println("woof");
+	
+}
+
+}
---
+++ src/oOP/DogTestClass.java
@@ -0,0 +1,10 @@
+package oOP;
+
+public class DogTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
