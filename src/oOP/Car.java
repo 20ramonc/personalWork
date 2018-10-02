dd58c2b4df0536c032eb22636ab50cc1fdbda88a
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-10-02 14:11:01 -0500

    Car

--- src/oOP/Car.java
+++ src/oOP/Car.java
@@ -1,5 +1,18 @@
 package oOP;
-import java.util.Scanner;
-public class Car {
 
+public class Car {
+	
+public Car()
+{
+}
+public void sayOdometer(){
+	System.out.println("0 miles");
+}
+
+public void sayFlat(){
+	System.out.println("You have a flat tire");
+}
+public void sayThirsty (){
+	System.out.println("You are out of gas");
+}
 }
