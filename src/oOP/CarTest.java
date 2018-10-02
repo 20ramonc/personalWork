641204755d57fac2c7f2bb46fa1a0d184c7af057
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-10-02 14:11:11 -0500

    CarTest

---
+++ src/oOP/CarTest.java
@@ -0,0 +1,17 @@
+package oOP;
+
+public class CarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Car Tesla = new Car ();
+Tesla.sayOdometer ();
+Car flat = new Car ();
+flat.sayFlat();
+Car thirsty = new Car ();
+thirsty.sayThirsty();
+
+
+	}
+
+}
