77dcc571a5b6209ae9c720c20f1513c165e53942
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-10-02 14:28:56 -0500

    Car Test update change

--- src/oOP/CarTest.java
+++ src/oOP/CarTest.java
@@ -1,11 +1,12 @@
 package oOP;
-
+import java.util.Scanner;
 public class CarTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Car Tesla = new Car ();
 Tesla.sayOdometer ();
+Scanner input = new Scanner(System.in); 
 Car flat = new Car ();
 flat.sayFlat();
 Car thirsty = new Car ();
