92745e89badec367adcf2b4b3519ad5e6539d5ed
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-10-04 14:12:22 -0500

    Parameter

---
+++ src/oOP/Parameter.java
@@ -0,0 +1,14 @@
+package oOP;
+
+public class Parameter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double num = 7.1;
+double num2 = (Math.pow(num, 3));
+System.out.println(num);
+num2= Math.floor(num);
+System.out.println(num2);
+
+
+}}
