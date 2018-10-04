00e8c69f11e1a9a8d9ad2c95731fe11230bec1cd
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-10-04 14:12:15 -0500

    Numbers

---
+++ src/oOP/Numbers.java
@@ -0,0 +1,12 @@
+package oOP;
+
+public class Numbers {
+public Numbers(){
+}
+public void sayNumber(double num) {	
+	System.out.println("Your number is: " + num); }
+
+public void sayNumbersPlus2(double num2) {
+	num2 = + 2;
+	System.out.println("Your number plus two is: " + num2);
+}}
---
+++ src/oOP/NumbersTest.java
@@ -0,0 +1,13 @@
+package oOP;
+
+public class NumbersTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Numbers numb = new Numbers ();
+numb.sayNumber (7.5);
+numb.sayNumbersPlus2 (9.8);
+numb.saySum (5.54,6.78);
+	}
+
+}
