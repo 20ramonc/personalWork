2d805789081926aff200876f539e824b3b6d02b5
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-10-05 13:45:58 -0500

    Parameter Change

--- src/oOP/Parameter.java
+++ src/oOP/Parameter.java
@@ -5,10 +5,10 @@ public class Parameter {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 double num = 7.1;
-double num2 = (Math.pow(num, 3));
-System.out.println(num);
-num2= Math.floor(num);
-System.out.println(num2);
+double num2 = 9.8;
+System.out.println(Math.pow(num, 3));
+System.out.println(Math.floor(num2));
+System.out.println(Math.ceil(num));
 
 
 }}
