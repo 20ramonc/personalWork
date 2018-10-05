0e2987102015d1dd8bc01d7b0327780369026f73
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-10-05 13:47:43 -0500

    Numbers Change

--- src/oOP/Numbers.java
+++ src/oOP/Numbers.java
@@ -3,10 +3,14 @@ package oOP;
 public class Numbers {
 public Numbers(){
 }
-public void sayNumber(double num) {	
-	System.out.println("Your number is: " + num); }
+public void sayNumber(double num1) {	
+	System.out.println("Your number is: " + num1); }
 
-public void sayNumbersPlus2(double num2) {
-	num2 = + 2;
-	System.out.println("Your number plus two is: " + num2);
+public void sayNumbersPlus2(double num1) {
+	System.out.println("Your number plus two is: " + (num1+2));
+	
+	}
+public void saySum (double num1, double num2) {
+	System.out.println("answer = " + (num1) + (num1+2));
+
 }}
