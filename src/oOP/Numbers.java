5602dd2eba84163fb2ea4076c3de523cd3553dae
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-10-09 15:03:56 -0500

    Numbers Change

--- src/oOP/Numbers.java
+++ src/oOP/Numbers.java
@@ -12,5 +12,17 @@ public void sayNumbersPlus2(double num1) {
 	}
 public void saySum (double num1, double num2) {
 	System.out.println("answer = " + (num1) + (num1+2));
-
-}}
+}
+public double returnSquare(double num1) 
+{
+	double answer = num1*num1;
+	return answer;	
+}
+public double returnArea(double length,double width) {
+	return length*width;
+}
+public int returnRoundUp(double number){
+	int temp =(int)Math.ceil(number);
+	return temp;
+}
+}
--- src/oOP/NumbersTest.java
+++ src/oOP/NumbersTest.java
@@ -8,6 +8,9 @@ Numbers numb = new Numbers ();
 numb.sayNumber (7.5);
 numb.sayNumbersPlus2 (9.8);
 numb.saySum (5.54,6.78);
+numb.returnSquare(7.0);
+numb.returnArea(8.0, 2.0);
+numb.returnRoundUp(9.8);
 	}
 
 }
