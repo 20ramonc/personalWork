eacceea64220419f754bf2c5c0aae2e17ed4f574
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-10-10 14:27:08 -0500

    Employee Update

--- src/oOP/Employee.java
+++ src/oOP/Employee.java
@@ -3,9 +3,10 @@ package oOP;
 public class Employee {
 	private double wage;
 	private int experience;
+	
 public Employee (){
-	wage = -999;
-	experience = -999;
+	wage = 15.00;
+	experience = 0;
 }
 public double getWage() {
 	return wage;
@@ -18,5 +19,9 @@ public void setWage(double newWage) {
 }
 public void setExperience(int numExperience) {
 	experience = numExperience;
+}
+public void raise() {
+	
+	wage = wage*1.1;
 }
 }
