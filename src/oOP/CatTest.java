0b80a9b2b54b3241f1bd17a49a9f6b3e5349cf55
Carli Ramon <20ramonc@elmbrookstudents.org>
2018-10-11 14:08:52 -0500

    cat update

--- src/oOP/CatTest.java
+++ src/oOP/CatTest.java
@@ -9,14 +9,14 @@ System.out.println("age: " + bob.getAge());
 bob.setAge(5);
 System.out.println("age: " + bob.getAge());
 System.out.println("hours slept: " + bob.getSleep());
-bob.setSleep(3);
+bob.setSleep(5);
 System.out.println("hours slept: " + bob.getSleep());
 bob.birthday();
-System.out.println("age: " + bob.getAge());
+System.out.println("birthday: " + bob.getAge());
 bob.SleepMore();
-System.out.println("hours slept: " + bob.getSleep());
+System.out.println("hours slept: " + bob.SleepMore());
 bob.SleepLess();
-System.out.println("hours slept: " + bob.getSleep());
+System.out.println("hours slept: " +bob.SleepLess());
 	}
 
 }
