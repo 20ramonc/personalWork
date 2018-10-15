8706243b1531a37169f3c430c2f530779274cab8
Unknown <20ramonc@elmbrookstudents.org>
2018-10-15 14:46:05 -0500

    Bank Test Update

--- src/oOP/BankTest.java
+++ src/oOP/BankTest.java
@@ -5,12 +5,23 @@ public class BankTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Bank savings = new Bank ();
-savings.deposit();
+System.out.println("savings Balance: " + savings.getBalance());
+savings.setBalance(150.00);
+System.out.println("savings Balance: " + savings.getBalance());
+System.out.println("interest rate: " + savings.getrate());
+savings.setrate(5.00);
+System.out.println("interest rate: " + savings.getrate());
 savings.interest();
-Bank checkings = new Bank ();
-checkings.withdrawl();
-checkings.charge();
 
+Bank checking = new Bank ();
+System.out.println("checking Balance: " + checking.getBalance());
+checking.setBalance(1150.00);
+System.out.println("checking Balance: " + checking.getBalance());
+System.out.println("interest rate: " + checking.getrate());
+checking.setrate(5.00);
+System.out.println("interest rate: " + checking.getrate());
+checking.withdrawl();
+checking.interest();
 	}
 
 }
