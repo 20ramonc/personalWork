387a835d34432f68e88008dbc964f2402d0d77f2
Unknown <20ramonc@elmbrookstudents.org>
2018-10-25 13:01:59 -0500

    activity6

---
+++ src/Loops/activity6.java
@@ -0,0 +1,20 @@
+package Loops;
+import java.util.Scanner;
+public class activity6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num = 0;
+int sum=0;
+Scanner input = new Scanner(System.in);
+System.out.println("Please enter a number. (9999 to quit):");
+num = input.nextInt();
+while (num !=9999) {
+sum=sum+num;
+System.out.println("Your number times two is: " + num);
+num=input.nextInt();
+}
+System.out.print("Your sum: " + sum);
+		}
+	}
+
