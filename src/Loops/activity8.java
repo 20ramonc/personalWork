ac45f3f039a0e7c44817d3495a798353e0448d76
Unknown <20ramonc@elmbrookstudents.org>
2018-10-25 13:02:10 -0500

    activity8

---
+++ src/Loops/activity8.java
@@ -0,0 +1,18 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class activity8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double value= 00;
+int value1=00;
+Scanner input = new Scanner(System.in);
+int value2=1;
+while(value!=9999) {
+	
+}
+	}
+
+}
