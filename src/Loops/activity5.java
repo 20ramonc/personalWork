ec138a0a745734188ebffe80ad4abb6336953c2d
Unknown <20ramonc@elmbrookstudents.org>
2018-10-26 11:12:22 -0500

    activity5

--- src/Loops/activity5.java
+++ src/Loops/activity5.java
@@ -5,14 +5,15 @@ public class activity5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int num = 0;
-int multiply=0;
+Scanner input = new Scanner(System.in);
+System.out.println("Please enter a number. (9999 to quit):");
+num = input.nextInt();
 while (num !=9999) {
-	Scanner input = new Scanner(System.in);
-	System.out.println("Please enter a number. (9999 to quit):");
-	int num1 = input.nextInt();
-	multiply=num1*2;
-	System.out.println("Your number times two is: " + multiply);
+System.out.println("number * 2: " + num*2);	
+System.out.println("Please enter a number. (9999 to quit):");
+num=input.nextInt();
 }
+
 
 	}
 
