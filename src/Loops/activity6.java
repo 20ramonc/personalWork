5386de0f3889dacff6c75345447332e2f1c7e462
Unknown <20ramonc@elmbrookstudents.org>
2018-10-26 11:12:31 -0500

    activity6

--- src/Loops/activity6.java
+++ src/Loops/activity6.java
@@ -8,11 +8,11 @@ int num = 0;
 int sum=0;
 Scanner input = new Scanner(System.in);
 System.out.println("Please enter a number. (9999 to quit):");
-num = input.nextInt();
-while (num !=9999) {
+num= input.nextInt();
+while (num != 9999) {
 sum=sum+num;
-System.out.println("Your number times two is: " + num);
-num=input.nextInt();
+System.out.println("Please enter a number. (9999 to quit):");
+num= input.nextInt();
 }
 System.out.print("Your sum: " + sum);
 		}
