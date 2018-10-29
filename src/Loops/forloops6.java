20529e042f0c42bedcf5b59ef7c2a7099cd93631
Unknown <20ramonc@elmbrookstudents.org>
2018-10-29 14:35:04 -0500

    loops6

---
+++ src/Loops/forloops6.java
@@ -0,0 +1,16 @@
+package Loops;
+import java.util.Scanner;
+public class forloops6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int counter=0;
+int total=0;
+Scanner input = new Scanner (System.in);
+for(counter=1; counter<=10; counter=counter++) {
+	total+=counter;
+		System.out.println("sum of digits: " + total);
+	} counter = input.nextInt();
+
+}
+}
