b0e76bf34e16e56be83ad95d046054912b16f849
Unknown <20ramonc@elmbrookstudents.org>
2018-10-29 14:34:35 -0500

    loops

---
+++ src/Loops/forloops.java
@@ -0,0 +1,28 @@
+package Loops;
+
+public class forloops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int counter=0;
+for(counter=1; counter<=5; counter=counter+1) {
+	System.out.println(counter);
+}
+for(int j=0;j<5;j++) {
+	System.out.println(j);
+}
+
+for(counter=1; counter<=10; counter=counter+1) {
+System.out.println(counter+" hello");
+	}
+for(counter=1; counter<=10; counter=counter+2) {
+	System.out.println(counter+" hello");
+}
+for(counter=15; counter<=60; counter=counter+5) {
+	System.out.println(counter+" hello");
+}
+for(int j=10;j>0;j--) {
+	System.out.println(j+" hello");
+}
+}
+}
