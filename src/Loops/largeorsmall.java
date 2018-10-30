5514560b0f4a5226a50d5b8950d08d447418e683
Unknown <20ramonc@elmbrookstudents.org>
2018-10-30 14:50:52 -0500

    large or small

---
+++ src/Loops/largeorsmall.java
@@ -0,0 +1,27 @@
+package Loops;
+import java.util.Scanner;
+public class largeorsmall {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int largest=0;
+int smallest=99999;
+Scanner input = new Scanner(System.in);
+System.out.println("enter test score: ");
+int test=input.nextInt();
+while(test!=9999){
+	if(test>=largest) {
+		largest=test;
+		System.out.println(largest);
+	}
+	if(test<smallest) {
+	smallest=test;
+	}
+	System.out.println("Please enter test(9999 to quit)");
+	test = input.nextInt();
+}
+	System.out.println("largest" +largest);
+	System.out.println("smallest" + smallest);
+}
+	}
+
