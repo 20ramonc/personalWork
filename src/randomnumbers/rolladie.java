8a6e190bc461fd3a212b790258ca24d904893848
Unknown <20ramonc@elmbrookstudents.org>
2018-11-08 12:53:07 -0600

    roll  a die

--- src/randomnumbers/rolladie.java
+++ src/randomnumbers/rolladie.java
@@ -6,20 +6,19 @@ public class rolladie {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Random generator = new Random();
-int min=0;
-int max=0;
-int roll=0;
+
 int randomNum=0;
 System.out.println("Number of sides" );
 Scanner input = new Scanner(System.in);
-int sides = input.nextInt();
+int max = input.nextInt();
+int min=1;
 System.out.println("Number of rolls");
 int rolls = input.nextInt();
-while(randomNum<sides) {
+for(int roll=1; roll<=rolls; roll++) {
 	randomNum=min+generator.nextInt(max-min+1);
-}
-	System.out.println(" you rolled a " + randomNum);
+
+	System.out.println("you rolled a " +randomNum);
 }
 	}
-
+}
 
