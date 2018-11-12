857f7867f477abbb5a94aca875a5b45fd8e6f82a
Unknown <20ramonc@elmbrookstudents.org>
2018-11-12 14:55:24 -0600

    Name and Age change

--- src/Strings/NameAge.java
+++ src/Strings/NameAge.java
@@ -1,9 +1,17 @@
 package Strings;
-
+import java.util.Scanner;
 public class NameAge {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+System.out.println("Age?");
+int age=input.nextInt();
+
+input.nextLine();
+
+System.out.println("Name?");
+String name = input.nextLine();
 
 	}
 
