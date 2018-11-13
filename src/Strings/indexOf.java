509f1f22c601bf6d4b23d56516704fc3d553f878
Unknown <20ramonc@elmbrookstudents.org>
2018-11-13 14:27:53 -0600

    indexOf

---
+++ src/Strings/indexOf.java
@@ -0,0 +1,14 @@
+package Strings;
+import java.util.Scanner;
+public class indexOf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+System.out.println("sentence");
+String sentence = input.nextLine();
+System.out.print("The letter X first ocurs at index " + sentence.indexOf("X"));
+
+	}
+
+}
