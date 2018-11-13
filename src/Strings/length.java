c8a445065445b509392f50c8e41d548a5d83ec93
Unknown <20ramonc@elmbrookstudents.org>
2018-11-13 14:29:52 -0600

    length

---
+++ src/Strings/length.java
@@ -0,0 +1,13 @@
+package Strings;
+import java.util.Scanner;
+public class length {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+System.out.println("sentence");
+String sentence = input.nextLine();
+System.out.println("number of characters: " + sentence.length());
+	}
+
+}
