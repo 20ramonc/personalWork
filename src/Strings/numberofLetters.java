ac4060dac561a024ec16b7f9013d12a7f9b353dd
Unknown <20ramonc@elmbrookstudents.org>
2018-11-15 12:56:52 -0600

    number of letters

---
+++ src/Strings/numberofLetters.java
@@ -0,0 +1,14 @@
+package Strings;
+import java.util.Scanner;
+public class numberofLetters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+System.out.println("sentence");
+String sentence = input.nextLine();
+
+
+	}
+
+}
