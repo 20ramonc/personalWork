57a068d6f9efbccbe4a550215ebafc15793e5d66
Unknown <20ramonc@elmbrookstudents.org>
2018-11-27 14:25:19 -0600

    roll a die change

--- src/arrays/rolladie.java
+++ src/arrays/rolladie.java
@@ -6,13 +6,9 @@ public class rolladie {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Scanner input = new Scanner(System.in);
-Random generator = new Random();
-int max=6;
-int min=1;
 int [] roll = new int [10000];
 int num=0;
-for(int j=0; j<10000; j++){
-	roll[j]=min+generator.nextInt(max-min+1);
+for(int j=0; j<roll.length; j++){
 	System.out.println("number");
 	num=input.nextInt();
 }while(num!=9999);
