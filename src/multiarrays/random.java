d2f4240f36b7234a9a84a2dd0dccb812f92d480c
Unknown <20ramonc@elmbrookstudents.org>
2018-12-04 14:51:37 -0600

    random Numbers 4x3

--- src/multiarrays/random.java
+++ src/multiarrays/random.java
@@ -1,15 +1,18 @@
 package multiarrays;
-
+import java.util.Random;
 public class random {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-final int maxrow=9;
-final int maxcolumn=14;
+Random generator = new Random();
+int sum=0;
+final int maxrow=3;
+final int maxcolumn=7;
 int [] [] random = new int [maxrow][maxcolumn];
 
 for(int column=0; column<maxcolumn; column++){
 	for(int row=0; row<maxrow; row++ ){
+		int randomNum=0+generator.nextInt(100-0+1);
 		System.out.println(random[row][column]+ " ");
 	}
 	System.out.println();
