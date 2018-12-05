d40c674abd846341833958e39aba22c866e15627
Unknown <20ramonc@elmbrookstudents.org>
2018-12-05 15:07:16 -0600

    three names update

--- src/arraysList/threenames.java
+++ src/arraysList/threenames.java
@@ -5,9 +5,13 @@ public class threenames {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 ArrayList<String> names = new ArrayList<String>();
-names.add("Bob");
+names.add("Tom");
 names.add("Joe");
 names.add("Sue");
+names.add("Bob");
+names.add("Jane");
+names.add(0, "Billy");
+
 	}
 
 }
