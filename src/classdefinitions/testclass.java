2610e8c84921f76eae53c7b23cfd61c396aa5d8e
Unknown <20ramonc@elmbrookstudents.org>
2018-12-14 15:05:32 -0600

    recursion test class

---
+++ src/classdefinitions/testclass.java
@@ -0,0 +1,16 @@
+package classdefinitions;
+
+public class testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(sumNum(10));
+	}
+public static int sumNum (int num){
+	if(num>1){
+		return (num+sumNum(num-1));
+}
+	return 0;
+	}
+
+}
