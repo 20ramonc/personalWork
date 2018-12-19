26441bbd708544f8f87c8f0481617b8ffc3834a8
Unknown <20ramonc@elmbrookstudents.org>
2018-12-19 13:49:37 -0600

    book class

---
+++ src/designingclasses/book.java
@@ -0,0 +1,21 @@
+package designingclasses;
+
+public class book {
+private String title;
+private int pages;
+
+public book () {
+	title="title";
+	pages=100000;
+}
+public book(String xtitle, int xpages){
+	title=xtitle;
+	pages=xpages;
+}
+public void getTitle(){
+	return;
+}
+public void getPages () {
+	return;
+}
+}
