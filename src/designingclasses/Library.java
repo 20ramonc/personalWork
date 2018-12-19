a1c023ae0e199b5af9e430040f96044d8d6ed226
Unknown <20ramonc@elmbrookstudents.org>
2018-12-19 15:09:12 -0600

    library

--- src/designingclasses/Library.java
+++ src/designingclasses/Library.java
@@ -1,5 +1,30 @@
 package designingclasses;
-
+import java.util.ArrayList;
 public class Library {
+private String name;
+private ArrayList<book>Books;
+
+public Library () {
+	name="School's Library";
+for(int j=0; j<10; j++){
+	Books.add(new book());
+}
+}
+public Library (String xname, ArrayList<book>xBooks) {
+	name=xname;
+	Books=xBooks;
+}
+public void checkout() {
+	for(int j=Books.size()-1; j>=0; j--){
+		book remove = Books.remove(1);
+	}
+}
+public void returnbook (){
+	Books.add(new book());
+}
+public void fire () {
+	Books.removeAll(Books);
+	
+}
 
 }
