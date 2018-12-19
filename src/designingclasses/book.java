44b0361f34ae1a476d9d3a8d03e01210bd38b1c6
Unknown <20ramonc@elmbrookstudents.org>
2018-12-19 14:53:34 -0600

    book class change

--- src/designingclasses/book.java
+++ src/designingclasses/book.java
@@ -6,16 +6,29 @@ private int pages;
 
 public book () {
 	title="title";
-	pages=100000;
+	pages=1000000;
 }
 public book(String xtitle, int xpages){
 	title=xtitle;
-	pages=xpages;
+	if(pages>0){
+		xpages=pages;
+	}else{
+		pages=xpages;
+	}
 }
-public void getTitle(){
-	return;
+public String getTitle(){
+	return title;
 }
-public void getPages () {
-	return;
+public int getPages () {
+	return pages;
+}
+public String toString(){
+	return "Book object: title= " + title + "number of pages= "+ pages;
+}
+public boolean equals(book otherObject){
+	return(this.toString().equals(otherObject.toString()));
+}
+public book clone() {
+	return new book(title,pages);
 }
 }
