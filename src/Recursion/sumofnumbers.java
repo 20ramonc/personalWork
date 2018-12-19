a9d3fc2346f77ee5f4c2dd0e985e2ec7c9e611f0
Unknown <20ramonc@elmbrookstudents.org>
2018-12-19 13:47:25 -0600

    sum of numbers

--- src/Recursion/sumofnumbers.java
+++ src/Recursion/sumofnumbers.java
@@ -4,7 +4,13 @@ public class sumofnumbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(sumNum(6));
+	}
+public static int sumNum (int num){
+	if(num>0){
+		return (num+sumNum(num-1));
+}
+	return 0;
 	}
 
 }
