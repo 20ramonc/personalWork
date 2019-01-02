cf2e40ccaf1b27e1d86208f5e412587d0d97631c
Unknown <20ramonc@elmbrookstudents.org>
2019-01-02 14:19:35 -0600

    deleted designing classes: employee class

--- src/designingclasses/employeeclass.java
+++
@@ -1,9 +0,0 @@
-package designingclasses;
-
-public class employeeclass {
-private String name;
-private double rate;
-private double [] worked;
-private static int employeeclassnumber;
-private 
-}
